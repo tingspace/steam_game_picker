3a096a1a45d91c959ae91adb5d65fa82773a7053
tingspace <ting.space@outlook.com>
2023-09-20 23:55:31 +1000

    Remove todo from script file

--- random_game.rb
+++ random_game.rb
@@ -1,16 +1,3 @@
-# Note: You can use the Windows Registry to find the Steam Install Path
-# HOWEVER
-# I am too lazy to learn the Win32 API just for this shitty script
-
-# What to do:
-# 1. Check for config file
-# 2. If config file doesn't exist, prompt for user input for Steam Install path
-# 3. Find libraryfolders.vdf file
-# 4. Parse file to get library locations
-# 5. Open location to read files
-# 6. Find files in this format: appmanifest_123.acf
-# 7. Compile the list of games
-
 require 'yaml'
 require_relative './lib/vdf_ruby/parser'
 
