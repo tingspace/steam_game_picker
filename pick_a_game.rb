4695fcd1ffdbf58968fce1d3eb7afe30023dff88
tingspace <ting.space@outlook.com>
2023-09-26 03:07:38 +1000

    Ruby 1-liners make me happy

--- pick_a_game.rb
+++ pick_a_game.rb
@@ -2,9 +2,5 @@
 
 require_relative 'lib/games'
 
-games = if ARGV.empty?
-            Common.get_installed_games
-          else
-            Common.get_from_game_list(ARGV.first)
-        end
+games = ARGV.empty? ? Common.get_installed_games : Common.get_from_game_list(ARGV.first)
 system("start #{Common::STEAM_BASE_DATA_URI}/#{games[rand(0..games.length-1)]}")
